36bc12788ef69de770ce3edd27cecb7138e5616a
ivatra <ivatra@yandex.ru>
2023-01-14 23:13:38 +0700

    refactored something

--- backend/sheduler/checkUserActivity.js
+++ backend/sheduler/checkUserActivity.js
@@ -1,7 +1,6 @@
+const { User } = require('../models/userModel')
 const mongoClient = require("../mongo");
 const requests = mongoClient.db('Messenger').collection('usersRequests');
-const { User } = require('../models/userModel')
-
 
 async function findActiveUsers() {
     return await User.findAll({
@@ -9,13 +8,17 @@ async function findActiveUsers() {
         attributes: ['id']
     })
 }
+
+function minutesToTimeSwamp(minutes){
+    return 1000 * 60 * minutes
+}
 async function checkUserActivity() {
     const activeUsers = await findActiveUsers();
     const ids = activeUsers.map(user => user._id);
     
     const inActiveUsers = await requests.find({
         _id: { $in: ids },
-        lastRequestTime: { $lt: Date.now() - 300000 }
+        lastRequestTime: { $lt: Date.now() - minutesToTimeSwamp(5) }
     }).toArray();
 
     inActiveUsers.forEach(async (user) => {
