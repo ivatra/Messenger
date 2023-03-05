d57a84cc198d7b50ec09dca68e751ba5da6deb88
ivatra <ivatra@yandex.ru>
2023-03-05 23:57:17 +0700

    inicialized next project

---
+++ core/frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          {'Hello world!'}
+        </div>
+      </main>
+    </>
+  )
+}
